7e697ebdba0a6e82804e16a92e186993f4a06093
francofriz <ffriz2010@gmail.com>
2019-09-20 18:33:20 +0200

    Apple - Includes

--- pacman.cpp
+++ pacman.cpp
@@ -1,2 +1,9 @@
-#include <studio.h>
+#include <stdio.h>
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
 #include <GL/glut.h>
+#endif
